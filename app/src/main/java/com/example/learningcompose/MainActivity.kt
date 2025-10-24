6eb44b7373d1bd631ecd3b7969a7e3de55261320
BeatrisO <beatris_oliveira97@hotmail.com>
2025-10-24 14:24:07 -0300

    Incluir recurso de imagem no StudyCard

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,19 +27,27 @@ class MainActivity : ComponentActivity() {
 
 data class StudyMessage(val author: String, val body: String)
 
-@Composable
-fun StudyCard(msg: StudyMessage) {
-    Text(text = "Autora: ${msg.author}")
-    Text(text = msg.body)
-}
+    @Composable
+    fun StudyCard(msg: StudyMessage) {
+        Row {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+            )
+        }
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewStudyCard() {
-    StudyCard(
-        msg = StudyMessage(
-            author = "Beatris",
-            body = "O aprendizado de hoje é sobre como criar interfaces modernas com Compose!"
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewStudyCard() {
+        StudyCard(
+            msg = StudyMessage(
+                author = "Beatris",
+                body = "O aprendizado de hoje é sobre como criar interfaces modernas com Compose!"
+            )
         )
-    )
-}
+    }
