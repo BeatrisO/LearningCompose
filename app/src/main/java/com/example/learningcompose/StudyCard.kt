5f25d9f87aa41edf54989dce6c1509af885c97f3
BeatrisO <beatris_oliveira97@hotmail.com>
2025-10-31 14:49:19 -0300

    Implementa animação para expandir mensagens, alterando cor e tamanho do conteúdo com Compose

--- app/src/main/java/com/example/learningcompose/StudyCard.kt
+++ app/src/main/java/com/example/learningcompose/StudyCard.kt
@@ -1,16 +1,24 @@
 package com.example.learningcompose
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learningcompose.ui.theme.LearningComposeTheme
@@ -20,6 +28,7 @@ data class StudyMessage(val author: String, val body: String)
 @Composable
 fun StudyCard(msg: StudyMessage) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
+
         Image(
             painter = painterResource(R.drawable.profile_picture),
             contentDescription = "Foto de perfil da autora",
@@ -28,25 +37,40 @@ fun StudyCard(msg: StudyMessage) {
                 .clip(CircleShape)
                 .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
-
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        Column(modifier = Modifier.clickable {}) {
+
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleMedium
             )
+
             Spacer(modifier = Modifier.height(4.dp))
+
+            var isExpanded by remember { mutableStateOf(false) }
+
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.surfaceVariant,
+                label = "Surface color animation"
+            )
+
             Surface(
                 shape = MaterialTheme.shapes.medium,
                 shadowElevation = 1.dp,
-                color = MaterialTheme.colorScheme.surfaceVariant
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .clickable { isExpanded = !isExpanded }
             ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 8.dp),
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
